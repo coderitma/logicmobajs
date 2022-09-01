1b85f2e043cdb96090037b409243ebd16a18b257
Yanzen <yanwarsolah@gmail.com>
2022-09-01 21:12:39 +0700

    game class 1.0

--- script.js
+++ script.js
@@ -1,46 +1,78 @@
-class BaseUser {
-    constructor (username, password) {
-        this.username = username;
-        this.password = password;
-    }
+class Hero {
+  #nama;
+  constructor (nama) {
+    this.#nama = nama;
+    this.jenis = "";
+    this.skin = "";
+    this.blood = 0;
+    this.damage = 0;
+    this.speed = 0
+  }
 
-    login(username, password) {
-        
-    }
+  set nama(nama) {
+    this.#nama = nama;
+  }
 
-    logout() {
-
-    }
-
-    register() {
-
-    }
+  get nama() {
+    return this.#nama;
+  }
 }
 
-
-class Admin extends BaseUser {
-    login(username, password) {
-        if (this.password == password && this.username == username) {
-            return "redirect ke admin"
-        } else {
-            return "gagal";
-        }
+class Player extends Hero {
+  move(direction) {}
+  attack(enemy) {
+    let status = "attack"
+    let winner = ""
+    
+    if (enemy.blood - this.damage > 0) {
+      enemy.blood = enemy.blood - this.damage
+    } else {
+      enemy.blood = 0;
+      winner = this.nama
     }
+
+    return {
+      "status": status,
+      "attack_from": this.nama,
+      "damage": this.damage,
+      "received_by": enemy.nama,
+      "current_blood": enemy.blood,
+      "winner": winner
+    }
+    // cek apakah blood enemy saat ini masih memungkinakn 
+    // untuk dikurangi dengan damage player?
+    // kalo iya.. yaudah:
+    // enemny blood akan dikurangi dengan jumlah
+    // damage-nya si player
+    // kalo kaga.. maka:
+    // enemy kalah!
+  }
+  attacked(enemy) {
+    // cek apakah blood player saat ini masih memungkinkan
+    // untuk dikurangi dengan damage-nya?
+    // kalo iya.. maka:
+    // blood player akan dikurangi dengan jumlah
+    // damage-nya si enemy
+    // kalau tidak.. maka:
+    // player kalah!
+  }
 }
 
-class Employee extends BaseUser {
-    login(username, password) {
-        if (this.password == password && this.username == username) {
-            return "redirect ke dashboard employee"
-        } else {
-            return "gagal";
-        }
-    }
-}
+const wanwan = new Player("wanwan")
+wanwan.jenis = "marksman"
+wanwan.skin = "pink"
+wanwan.blood = 100;
+wanwan.damage = 60;
+wanwan.speed = 90;
 
 
-const budi = new Employee("budibudi", "123qwe")
-const broto = new Admin("brotonomus", "321ewq")
+const ling = new Player("ling")
+ling.jenis = "assasin"
+ling.skin = "blue"
+ling.blood = 100;
+ling.damage = 75;
+ling.speed = 90;
 
-console.log(budi.login("budibudi", "123qwe"))
-console.log(broto.login("brotonomus", "321ewq"))
+
+let attackResult = wanwan.attack(ling)
+console.table(attackResult);
