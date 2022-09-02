be937015b67d222a8948774437466504b7bc7f6e
Yanzen <yanwarsolah@gmail.com>
2022-09-02 23:11:41 +0700

    kirimnotif v1.0.0

--- script.js
+++ script.js
@@ -1,27 +1,12 @@
-var btnAdd = document.getElementById("add")
-var btnClear = document.getElementById("clear")
-var inputTugas = document.getElementById("inputTugas")
-var ulDaftarTugas = document.getElementById("daftarTugas")
-var daftarTugas ="";
-var counterID = 0;
+document.getElementById("kirim").addEventListener('click', function (e) {
 
-btnAdd.addEventListener('click', function (event) {
-  let hasil = inputTugas.value;
-  inputTugas.value = ""
-  let li = document.createElement("li");
-  let text = document.createTextNode(hasil);
-  li.appendChild(text);
-  li.setAttribute("class", "list-group-item");
-  li.setAttribute("onclick", "apus(this)");
-  ulDaftarTugas.appendChild(li);
+  let value1 = document.getElementById("inputNama").value
+  let value2 = document.getElementById("inputPesan").value
+
+  let url = `?value1=${value1}&value2=${value2}`;
+
+  fetch(url, {
+    method: 'GET',
+    mode: 'no-cors',
+  }).then(response => console.log("data ke kirim"))
 })
-
-btnClear.addEventListener('click', function (event) {
-  daftarTugas = []
-  inputTugas.value = ""
-  ulDaftarTugas.innerHTML = daftarTugas.join("")
-})
-
-function apus(element) {
-  ulDaftarTugas.removeChild(element);
-}
